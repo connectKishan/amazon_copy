8416a0f0cc813825c222ad89f17462bc1c632d89
ispiron-kishan yadav <connect.kishanyadav@gmail.com>
2017-10-11 16:51:47 +0530

    category and products added

--- server.js
+++ server.js
@@ -14,8 +14,10 @@ var passport=require('passport');
 
 
 
+
 var secret=require('./config/secret');
 var User=require('./models/users');
+var Category = require('./models/category');
 
 mongoose.connect(secret.database,function(err){
     if(err){
@@ -43,7 +45,17 @@ app.use(passport.session());
 app.use(function(req,res,next){
     res.locals.user=req.user;
     next();
-})
+});
+
+app.use(function(req, res, next) {
+    Category.find({}, function(err, categories) {
+      if (err) return next(err);
+      res.locals.categories = categories;
+      next();
+    });
+  });
+  
+
 
 
 app.engine('ejs',engine);
@@ -51,10 +63,14 @@ app.set('view engine','ejs');
 
 
 var mainRoutes=require('./routes/main');
-var userRoute=require('./routes/user')
+var userRoute=require('./routes/user');
+var adminRoute=require('./routes/admin');
+var apiRoutes=require('./api/api')
 
 app.use(mainRoutes);
 app.use(userRoute);
+app.use(adminRoute);
+app.use('/api',apiRoutes);
 
 app.listen(secret.port,function(err){
     if(err) throw err;
